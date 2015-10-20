68d484876051cd3692ecff2922f9352ce81dc758
eamexicano <emmanuelayalamexicano@gmail.com>
2015-10-19 20:20:01 -0500

    Incluir app de referencia para leer / escribir archivo de etiquetas

---
+++ app/index.php
@@ -0,0 +1,155 @@
+  <?php
+  
+      /*
+        Si se quiere trabajar con caracteres Unicode
+      */
+        mb_internal_encoding('UTF-8');
+        mb_http_output('UTF-8');
+        mb_http_input('UTF-8');
+        mb_language('uni');
+        mb_regex_encoding('UTF-8');
+        ob_start('mb_output_handler');
+        /*
+          Si se quiere trabajar con caracteres Unicode
+        */
+  
+  
+    if (isset($_POST['actualizar'])) {
+      
+        unset($_POST['actualizar']);
+
+        $data = $_POST;
+      
+        $content = "<?php\n\n";
+        $content .= "\$tag = array(\n\n";
+
+        foreach ($data as $key => $value) {
+          $content .= "\t\"$key\" => array(\n";
+          foreach ($value as $k => $v) {        
+            $content .=  "\t\t\"$k\" => \"$v\",\n";
+          }
+          $fix = rtrim($content, ",\n");
+          $content = $fix . "\n";
+          $content .= "\t),\n\n";
+        }
+
+        $fix = rtrim($content, ",\n\n");
+        $content = $fix . "\n\n";
+        $content .= ")";
+        $content .= "\n?>";
+  
+        $archivo = fopen("lib/tags.php", 'w') or die('No se pudo crear el archivo log.txt');
+        fwrite($archivo, $content);
+        fclose($archivo);
+        
+        
+    } 
+
+    require_once 'lib/tags.php';
+
+?>
+<!DOCTYPE html>
+<html lang='es-MX'>
+	<head>
+    <meta charset='utf-8'>
+    <meta name=viewport content="width=device-width, initial-scale=1">    
+    <title><?php echo $tag['document']['title']; ?></title>
+    <style>
+        html {
+          width: 100%;
+          height: 100%;
+          background: white;
+          font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+          line-height: 1.44;
+          color: #151515;
+        }
+    
+        body {
+          margin: 0 auto;
+          width: 640px;        
+          padding: 16px;
+        }
+    
+        label {
+          display: block;
+          margin: 8px 0;
+          font-weight: bold;
+        }
+    
+        textarea {
+          display: block;
+          width: 100%;
+          border-radius: 2px;
+          border: 1px solid #c0c0c0;
+          font-size: 16px;
+          padding: 8px 0;
+        }
+    
+        .existente {
+          display: block;
+          margin: 8px 0;
+          color: #454545;      
+        }
+    
+        footer {
+          text-align: right;
+        }
+    
+        input[type='submit'] {
+          display: block;
+          margin: 32px auto;
+          padding: 8px 16px;
+          font-size: 16px;
+          border-radius: 2px;
+          background: #f2f2f2;
+          border: 1px solid #c0c0c0;
+        }
+    </style>
+	</head>
+	<body>
+		<header class='header'>      
+      <?php echo $tag['document']['header']; ?>
+		</header>
+
+		<main class='content'>
+      <div class='container'>            
+        <?php echo $tag['page']['copy']; ?>
+
+<hr>
+
+<form action='index.php' method='post' onsubmit='return validar();' >
+  <?php
+    
+  foreach ($tag as $key => $value) {
+    echo "<h2>" . $key . "</h2>";
+    foreach ($value as $k => $v) {
+      echo "<label>" . $k . "</label>" ;
+      echo "<div class='existente'>" . htmlentities(stripslashes($v)) . "</div>";
+      echo "<textarea name='" . $key . "[" . $k . "]'>" . htmlentities(stripslashes($v)) . "</textarea>";
+    }
+  }
+    
+  ?>
+	<input type='submit' value='<?php echo $tag["page"]["actualizar_etiqueta"]; ?>' name='actualizar' />
+</form>
+
+<script>
+    function validar() {
+      return confirm("<?php echo $tag['javascript']['confirm']; ?>");
+    }
+  
+</script>
+	 
+      </div>
+      </main>
+
+			<footer class='footer'>
+				<p>
+          <br>
+          <?php echo $tag['document']['footer']; ?>
+          <br>
+				</p>
+			</footer>
+	</body>
+</html>
+
---
+++ app/lib/tags.php
@@ -0,0 +1,21 @@
+<?php
+
+$tag = array(
+
+	"document" => array(
+		"title" => "Marbete <=> Tag",
+		"header" => "<h1>Marbete <=> Tag</h1>",
+		"footer" => "<p><a href='//www.eamexicano.com' target='_blank'>eamexicano</a></p>"
+	),
+
+	"page" => array(
+		"copy" => "<p>Las etiquetas de la interfaz gráfica y de algunos mensajes de la aplicación están almacenadas en el archivo: <em>lib/tags.php</em><br>Puedes editar el archivo directamente o puedes modificar el contenido de las traducciones desde este documento.<br>Puedes utilizar etiquetas HTML para dar formato al contenido.<br>Si utilizas atributos para las etiquetas utiliza comillas simples - apóstrofes - para que se interprete correctamente.<br>Una vez enviado el formulario vuelve a recargar la página para que se visualice el contenido actualizado.<br></p>",
+		"actualizar_etiqueta" => "Actualizar Etiquetas"
+	),
+
+	"javascript" => array(
+		"confirm" => "Al enviar el formulario se va a sobrescribr el documento lib/tag.php con los valores actuales. ¿Quieres continuar?"
+	)
+
+)
+?>
